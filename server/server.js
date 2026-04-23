fc978ef8ecd4a76504ca0d6ed55de813180ec556
vickynina006 <142887080+vickynina006@users.noreply.github.com>
2026-04-23 03:05:30 +0100

    Update CORS origin to production URL

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import userDataRouter from "./routes/userDataRoute.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://authra-ten.vercel.app", credentials: true }));
 app.use(cookieParser());
 
 connectDB();
