67b445a4b1c63f12506da5f25ef93f814e22d481
Vickynina <vickynina006@gmail.com>
2026-05-07 13:42:53 +1200

    Update cookie settings for secure handling in registration and login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,8 +23,8 @@ export const register = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none" ,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -77,8 +77,8 @@ export const login = async (req, res) => {
     // });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
     // const {
